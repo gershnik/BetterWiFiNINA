e0e8ed70a5a0a3489f0e1146edf43f65eb6819ad
Eugene Gershnik <gershnik@users.noreply.github.com>
2024-04-03 11:51:14 -0700

    Updating AP_SimpleWebServer sample to use plain sockets

--- examples/AP_SimpleWebServer/AP_SimpleWebServer.ino
+++ examples/AP_SimpleWebServer/AP_SimpleWebServer.ino
@@ -2,7 +2,7 @@
   WiFi Web Server LED Blink
 
   A simple web server that lets you blink an LED via the web.
-  This sketch will create a new access point (with no password).
+  This sketch will create a new access point.
   It will then launch a new server and print out the IP address
   to the Serial Monitor. From there, you can open that address in a web browser
   to turn on and off the LED on pin 13.
@@ -11,22 +11,19 @@
     http://yourAddress/H turns the LED on
     http://yourAddress/L turns it off
 
-  created 25 Nov 2012
-  by Tom Igoe
-  adapted to WiFi AP by Adafruit
  */
 
 #include <SPI.h>
 #include <BetterWiFiNINA.h>
 #include "arduino_secrets.h" 
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
-char ssid[] = SECRET_SSID;        // your network SSID (name)
-char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+const char ssid[] = SECRET_SSID;        // your network SSID (name)
+const char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
 int keyIndex = 0;                // your network key index number (needed only for WEP)
 
 int led =  LED_BUILTIN;
 int status = WL_IDLE_STATUS;
-WiFiServer server(80);
+WiFiSocket serverSocket;
 
 void setup() {
   //Initialize serial and wait for port to open:
@@ -70,11 +67,28 @@ void setup() {
   // wait 10 seconds for connection:
   delay(10000);
 
-  // start the web server on port 80
-  server.begin();
-
   // you're connected now, so print out the status
   printWiFiStatus();
+
+  // create server socket
+  serverSocket = WiFiSocket(WiFiSocket::Type::Stream, WiFiSocket::Protocol::TCP);
+  if (!serverSocket) {
+    Serial.print("Creating server socket failed: error ");
+    Serial.println(WiFiSocket::lastError());
+    // don't continue
+    while (true);
+  }
+  //Bind to port 80
+  if (!serverSocket.bind(80)) {
+    Serial.print("Binding server socket failed: error ");
+    Serial.println(WiFiSocket::lastError());
+  }
+
+  //And start listening
+  if (!serverSocket.listen(5)) {
+    Serial.print("Listen on server socket failed: error ");
+    Serial.println(WiFiSocket::lastError());
+  }
 }
 
 
@@ -92,58 +106,98 @@ void loop() {
       Serial.println("Device disconnected from AP");
     }
   }
-  
-  WiFiClient client = server.available();   // listen for incoming clients
 
-  if (client) {                             // if you get a client,
-    Serial.println("new client");           // print a message out the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      delayMicroseconds(10);                // This is required for the Arduino Nano RP2040 Connect - otherwise it will loop so fast that SPI will never be served.
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out to the serial monitor
-        if (c == '\n') {                    // if the byte is a newline character
+  //Accept incoming connection
+  IPAddress addr;
+  uint16_t port;
+  auto sessionSocket = serverSocket.accept(addr, port);
+  if (!sessionSocket) {
+    Serial.println("Accept on server socket failed: error ");
+    Serial.println(WiFiSocket::lastError());
+    delay(100);
+    return;
+  }
 
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println();
+  Serial.println("new client");
 
-            // the content of the HTTP response follows the header:
-            client.print("Click <a href=\"/H\">here</a> turn the LED on<br>");
-            client.print("Click <a href=\"/L\">here</a> turn the LED off<br>");
+  //set the session socket to non-blocking
+  if (!sessionSocket.setNonBlocking(true))  {
+    Serial.println("Setting socket to non-blocking failed: error ");
+    Serial.println(WiFiSocket::lastError());
+    delay(100);
+    return;
+  }
 
-            // The HTTP response ends with another blank line:
-            client.println();
-            // break out of the while loop:
-            break;
-          }
-          else {      // if you got a newline, then clear currentLine:
-            currentLine = "";
-          }
+  uint8_t buffer[256];       // a buffer to read to
+  String currentLine = "";   // a String to hold each request line from the client
+  bool doneReading = false;
+
+  //read until \n\r\n
+  while(!doneReading) {
+    auto read = sessionSocket.recv(buffer, sizeof(buffer));
+    if (read < 0) {
+      auto err = WiFiSocket::lastError();
+      if (err == EWOULDBLOCK)
+        continue;
+      Serial.println("reading from socket failed with error: ");
+      Serial.println(err);
+      delay(100);
+      return;
+    }
+    Serial.write(buffer, read); // print it out to the serial monitor
+    for(int i = 0; i != read; ++i) {
+      char c = buffer[i];
+      if (c == '\n') {                    // if the byte is a newline character
+        // if the current line is blank, you got two newline characters in a row.
+        // that's the end of the client HTTP request, so send a response:
+        if (currentLine.length() == 0) {
+          doneReading = true;
+          break;
+        } else {      // if you got a newline, then clear currentLine:
+          currentLine = "";
         }
-        else if (c != '\r') {    // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
+      } else if (c != '\r') {    // if you got anything else but a carriage return character,
+        currentLine += c;      // add it to the end of the currentLine
+      }
 
-        // Check to see if the client request was "GET /H" or "GET /L":
-        if (currentLine.endsWith("GET /H")) {
-          digitalWrite(led, HIGH);               // GET /H turns the LED on
-        }
-        if (currentLine.endsWith("GET /L")) {
-          digitalWrite(led, LOW);                // GET /L turns the LED off
-        }
+      // Check to see if the client request was "GET /H" or "GET /L":
+      if (currentLine.endsWith("GET /H")) {
+        digitalWrite(led, HIGH);               // GET /H turns the LED on
+      }
+      if (currentLine.endsWith("GET /L")) {
+        digitalWrite(led, LOW);                // GET /L turns the LED off
       }
     }
-    // close the connection:
-    client.stop();
-    Serial.println("client disconnected");
   }
+
+  //Our response
+  static const char response[] = 
+      "HTTP/1.1 200 OK\r\n"
+      "Content-Type: text/html\r\n"
+      "Connection: close\r\n"
+      "\r\n"
+      "<!DOCTYPE HTML>\n"
+      "<html><body>"
+      "Click <a href=\"/H\">here</a> turn the LED on<br>"
+      "Click <a href=\"/L\">here</a> turn the LED off<br>"
+      "</body></html>";
+
+  size_t written = 0;
+  while(written != sizeof(response)) {
+    auto sent = sessionSocket.send(response + written, sizeof(response) - written);
+    if (sent < 0) {
+      auto err = WiFiSocket::lastError();
+      if (err == EWOULDBLOCK)
+        continue;
+      Serial.println("writing to socket failed with error: ");
+      Serial.println(err);
+      delay(100);
+      return;
+    }
+    written += sent;
+  }
+
+  Serial.println("response sent");
 }
 
 void printWiFiStatus() {
